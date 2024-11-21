9d90c1fc0f499206ec36db6928b12eb9547260ee
sophiagatinha <162448760+sophiagatinha@users.noreply.github.com>
2024-11-21 09:06:50 -0300

    Add files via upload

---
+++ ferramenta de estudo- sophia/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz óculos em ingles',
+    'Óculos em ingles é (GLASSES)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokio'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual  capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz mesa em Inglês?',
+    'Mesa em ingles é table (TEIBOU)'
+)
